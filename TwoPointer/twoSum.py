e3c2495ccfb725cf7f2de0f534318305b3a7d0ce
lam-valamorgulis <80387108+lam-valamorgulis@users.noreply.github.com>
2023-09-30 12:42:56 +0000

    test

---
+++ TwoPointer/twoSum.py
@@ -0,0 +1,31 @@
+class Solution():
+
+  # this solution wil add more memory
+  # def twoSum(self, numbers, target):
+  #   table = {}
+  #   for i in range(len(numbers)) :
+  #     add_up_num = target - numbers[i]
+  #     if add_up_num not in table and numbers[i] <= target:
+  #       table[numbers[i]] = i
+  #     else:
+  #       return [table[add_up_num] + 1 , i + 1]
+
+  #  optimize the solition not using extra memory O(1) memory
+
+  def twoSum(self, numbers, target):
+    l, r = 0, len(numbers) - 1
+
+    while l < r:
+      sum = numbers[l] + numbers[r]
+      if sum > target:
+        r -= 1
+      if sum < target:
+        l += 1
+      else:
+        return [l + 1, r + 1]
+    return []
+
+
+solution = Solution()
+result = solution.twoSum([2, 7, 11, 15], 9)
+print(result)
