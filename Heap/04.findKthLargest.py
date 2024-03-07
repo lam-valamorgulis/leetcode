e12de277f7a89750fb4e1a9e67a1340d110f1386
lam-valamorgulis <80387108+lam-valamorgulis@users.noreply.github.com>
2024-03-07 14:58:02 +0000

    05.leastInterval.py

--- Heap/04.findKthLargest.py
+++ Heap/04.findKthLargest.py
@@ -1,16 +1,23 @@
-# https://leetcode.com/problems/kth-largest-element-in-an-array/
+# https://leetcode.com/problems/task-scheduler/
 
 import heapq
 
 
 class Solution:
 
-  def findKthLargest(self, nums: List[int], k: int) -> int:
-    max_heap = [-num for num in nums]
+  def leastInterval(self, tasks: List[str], n: int) -> int:
+    count = Counter(tasks)
+    maxHeap = [-cnt for cnt in count.values()]
+    heapq.heapify(maxHeap)
 
-    heapq.heapify(max_heap)
-
-    for _ in range(k - 1):
-      heapq.heappop(max_heap)
-
-    return -heapq.heappop(max_heap)
+    time = 0
+    q = deque()  # pairs of [-cnt, idleTime]
+    while maxHeap or q:
+      time += 1
+      if maxHeap:
+        cnt = 1 + heapq.heappop(maxHeap)
+        if cnt:
+          q.append([cnt, time + n])
+      if q and q[0][1] == time:
+        heapq.heappush(maxHeap, q.popleft()[0])
+    return time
