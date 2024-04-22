b0cffe2273935cf434dde96cf0d51bd2ead79879
lam-valamorgulis <80387108+lam-valamorgulis@users.noreply.github.com>
2024-04-22 15:43:41 +0000

    21.combinationSum4.py

--- DP/20.maximalSquare.py
+++ DP/20.maximalSquare.py
@@ -1,18 +1,15 @@
-# https://leetcode.com/problems/maximal-square/
+# https://leetcode.com/problems/combination-sum-iv/
+
 
 class Solution:
-  def maximalSquare(self, matrix: List[List[str]]) -> int:
-      if not matrix:
-          return 0
 
-      m, n = len(matrix[0]), len(matrix)
-      dp = [[0] * (m + 1) for _ in range(n + 1)]
-      max_side = 0
+  def combinationSum4(self, nums: List[int], target: int) -> int:
+    dp = [0] * (target + 1)
+    dp[0] = 1
 
-      for i in range(1, n + 1):
-          for j in range(1, m + 1):
-              if matrix[i - 1][j - 1] == "1":
-                  dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
-                  max_side = max(max_side, dp[i][j])
+    for i in range(1, target + 1):
+      for num in nums:
+        if num <= i:
+          dp[i] = dp[i] + dp[i - num]
 
-      return max_side * max_side
+    return dp[target]
---
+++ DP/21.combinationSum4.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/maximal-square/
+
+class Solution:
+  def maximalSquare(self, matrix: List[List[str]]) -> int:
+      if not matrix:
+          return 0
+
+      m, n = len(matrix[0]), len(matrix)
+      dp = [[0] * (m + 1) for _ in range(n + 1)]
+      max_side = 0
+
+      for i in range(1, n + 1):
+          for j in range(1, m + 1):
+              if matrix[i - 1][j - 1] == "1":
+                  dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                  max_side = max(max_side, dp[i][j])
+
+      return max_side * max_side
