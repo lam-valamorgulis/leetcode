03746949f209918878f9623c09be85caba9c47c8
lam-valamorgulis <80387108+lam-valamorgulis@users.noreply.github.com>
2024-05-02 16:10:17 +0000

    mctFromLeafValues

---
+++ Stack/5.mctFromLeafValues.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/
+
+
+class Solution:
+
+  def mctFromLeafValues(self, arr: List[int]) -> int:
+    stack = [float('inf')]
+    result = 0
+
+    for num in arr:
+      while stack[-1] <= num:
+        mid = stack.pop()
+        result += mid * min(stack[-1], num)
+      stack.append(num)
+
+    while len(stack) > 2:
+      result += stack.pop() * stack[-1]
+
+    return result
