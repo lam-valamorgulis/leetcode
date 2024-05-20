8bf981e257c65623560d24e30bb85e3fbad3f985
lam-valamorgulis <80387108+lam-valamorgulis@users.noreply.github.com>
2024-05-20 12:57:03 +0000

    tribonacci

--- DP/30.fibonacci.py
+++ DP/30.fibonacci.py
@@ -1,19 +1,17 @@
-# https://leetcode.com/problems/n-th-tribonacci-number/
+#https://leetcode.com/problems/pascals-triangle-ii/
 
 
 class Solution:
-
-    def tribonacci(self, n: int) -> int:
-        if n == 0:
+    def fib(self, n: int) -> int:
+        if n == 1 :
+            return 1
+        if n == 0 :
             return 0
-        if n == 1:
-            return 1
-        if n == 2:
-            return 1
-
         dp = [0] * (n + 1)
         dp[0] = 0
-        dp[1] = dp[2] = 1
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+        dp[1] = 1
+
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
         return dp[n]
